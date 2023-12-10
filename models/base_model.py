f40b840ec079d8a5f3188ba3df73a762af74a0ee
youssefhammani <youssefhammani04@gmail.com>
2023-12-10 11:16:04 -0800

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,6 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """
@@ -48,7 +47,6 @@ class BaseModel:
         with the current datetime.
         """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """
